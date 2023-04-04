e99a53845ea3726eb4290708c3820846cb727c91
john-adeojo <johnadeojo91@gmail.com>
2023-04-04 11:58:49 +0100

    clusters

--- scripts/.ipynb_checkpoints/cluster-checkpoint.py
+++ scripts/.ipynb_checkpoints/cluster-checkpoint.py
@@ -7,13 +7,16 @@ import plotly.graph_objects as go
 
 
 class ClusterAnalysis:
-    def __init__(self, dataframe, n_neighbors=15, min_cluster_size=5, min_dist=0.1, metric='euclidean', cluster_dims=None):
+    def __init__(self, dataframe, n_neighbors=15, min_cluster_size=5, min_dist=0.1, metric='euclidean', cluster_dims=None, export_data=False):
         self.dataframe = dataframe.copy()
         self.n_neighbors = n_neighbors
         self.min_cluster_size = min_cluster_size
         self.min_dist = min_dist
         self.metric = metric
         self.cluster_dims = cluster_dims
+        self.weight_class = self.dataframe['weight_class'].drop_duplicates().values[0]
+        self.export_data = export_data
+
 
     def perform_umap(self):
         reducer = UMAP(n_neighbors=self.n_neighbors, min_dist=self.min_dist, metric=self.metric, random_state=42)
@@ -25,20 +28,21 @@ class ClusterAnalysis:
         np.random.seed(42)
         clusterer = HDBSCAN(min_cluster_size=self.min_cluster_size, metric=self.metric)
         self.dataframe['cluster'] = clusterer.fit_predict(self.dataframe[['x', 'y']])
+        if self.export_data == True:
+            self.dataframe.to_csv(f"C:\\Users\\johna\\anaconda3\\envs\\ufc-env\\ufc_styles\\data\\02_intermediate\\fighter_cluster{ self.weight_class}")
+        
     
     def plot_scatter(self):
         unique_clusters = sorted(self.dataframe['cluster'].unique())
         fig = go.Figure()
         
-        weight_class = self.dataframe['weight_class'].drop_duplicates().values
-
         for cluster in unique_clusters:
             cluster_data = self.dataframe[self.dataframe['cluster'] == cluster]
             fig.add_trace(go.Scatter(x=cluster_data['x'], y=cluster_data['y'], mode='markers', name='Cluster ' + str(cluster),
                                      marker=dict(size=6, opacity=0.4), hovertext=cluster_data['Fighter_dims'],
                                      text=cluster_data['Fighter_dims'], textposition='top center', textfont=dict(size=10, color='black')))
 
-        fig.update_layout(title=f'Fighting Style Clusters: {weight_class}', showlegend=True, width=750, height=750)
+        fig.update_layout(title=f'Fighting Style Clusters: {self.weight_class}', showlegend=True, width=750, height=750)
         fig.show()
 
     def run(self):
