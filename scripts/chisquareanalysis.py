6376c05c9fc0cc54ed4297cbe77f4feb6b18c864
john-adeojo <johnadeojo91@gmail.com>
2023-04-04 23:12:15 +0100

    data updated

--- scripts/chisquareanalysis.py
+++ scripts/chisquareanalysis.py
@@ -9,9 +9,9 @@ class ChiSquareAnalysis:
 
     def run_chisquare_analysis(self, var):
         
-        self.df = self.df.loc[self.df['weight_class'] == self.category].copy()
+        data = self.df.loc[self.df['weight_class'] == self.category].copy()
         
-        contingency_table = pd.crosstab(self.df['style_matchup'], self.df[var])
+        contingency_table = pd.crosstab(data['style_matchup'], data[var])
 
         # Perform the Chi-square test
         chi2_stat, p_value, dof, ex = chi2_contingency(contingency_table)
@@ -21,10 +21,13 @@ class ChiSquareAnalysis:
         
         #standardized_residuals.index.name = None
         standardized_residuals = standardized_residuals.reset_index()
-
+        
+        print(self.category)
         print("Chi2 Stat:", chi2_stat)
         print("P Value:", p_value)
         print("Degrees of Freedom:", dof)
+        print(contingency_table)
+        print(standardized_residuals)
         # print("Expected Frequency Table:")
         # print(ex)
 
